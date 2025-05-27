04e174cbffea5eb7bf9023972414f0a376c1199e
Akenarin Komkoon <codingthailand@gmail.com>
2025-05-27 14:20:07 +0700

    add shadcnui

--- src/app/components/AppWelcome.tsx
+++ src/app/components/AppWelcome.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { Button } from "@/components/ui/button";
 import { useState } from "react";
 
 type AppWelcomeProps = {
@@ -21,6 +22,7 @@ export default function AppWelcome({ headTitle, isShow }: AppWelcomeProps ) {
         <>
            <h1>{headTitle}</h1>
            <p>{title.toUpperCase()}</p>
+           <Button onClick={handleClick}>Click Me</Button>
            <button className="bg-blue-700 p-3 m-3 text-white rounded-lg" onClick={handleClick}>กดได้เลย!</button>
            {currentYear}
            {

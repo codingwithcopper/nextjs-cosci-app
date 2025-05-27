61ff28df8018a7a142bf7d96fb0fba24e3926fb2
Akenarin Komkoon <codingthailand@gmail.com>
2025-05-27 11:19:02 +0700

    learn conditional rendering

--- src/app/components/AppWelcome.tsx
+++ src/app/components/AppWelcome.tsx
@@ -1,7 +1,18 @@
 export default function AppWelcome() {
+    const title = 'Welcome to cosci';
+    const currentYear = <p>2025</p>;
+    const isShow = true;
+
     return (
         <>
-           <p>Welcome to cosci</p> 
+           <p>{title.toUpperCase()}</p>
+           {currentYear}
+           {
+            isShow && <p>Date: 10/10/1998</p>
+           }
+           {
+            isShow ? <p>Hello Next.js</p> : <p>Hello JavaScript</p>
+           } 
         </>
     );
 }

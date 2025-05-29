81de2be7f2a9e3bd992308d8ea6e6a62e7f8a2e4
Akenarin Komkoon <codingthailand@gmail.com>
2025-05-29 11:57:57 +0700

    update

--- src/components/navbar-01/navbar-01.tsx
+++ src/components/navbar-01/navbar-01.tsx
@@ -3,6 +3,8 @@ import { Logo } from "./logo";
 import { NavMenu } from "./nav-menu";
 import { NavigationSheet } from "./navigation-sheet";
 import Link from "next/link";
+import { ShoppingBasket } from "lucide-react";
+import { Badge } from "../ui/badge";
 
 const Navbar01Page = () => {
   return (
@@ -15,6 +17,9 @@ const Navbar01Page = () => {
           <NavMenu className="hidden md:block" />
 
           <div className="flex items-center gap-3">
+
+            <Badge className="p-2 text-sm"><ShoppingBasket /> 0 item (s)</Badge>
+
             <Button asChild variant="outline" className="hidden sm:inline-flex">
               <Link href="/login">เข้าสู่ระบบ</Link>
             </Button>
